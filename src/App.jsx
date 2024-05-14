fd43ce6991658b28c3e32bca0e8503e0bc4d26e8
chromecruzer <shayammurali@gmail.com>
2024-05-14 11:20:13 +0530

    SCN

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <img src="/vite-deno.svg" alt="Vite with Deno" />
+     
+      <h1>Hellow Scanner..!!</h1>
+    </>
+  )
+}
+
+export default App
